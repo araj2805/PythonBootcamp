acb7b3de49432580ef751afba83204a3d253e823
Ankit Raj <araj2805@gmail.com>
2024-10-27 21:11:50 +0530

    Learn Variable and Types

---
+++ 01_basics/variable_and_types.py
@@ -0,0 +1,18 @@
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# print(mystring)
+# print(myfloat)
+# print(myint)
+
+if mystring == "hello":
+    print("String : %s" %mystring)
+
+print(float == 10.0)
+
+if isinstance(myfloat,float) and myfloat == 10.0:
+    print("Float Value : %f" %myfloat)
+
+if isinstance(myint,int) and myint == 20: 
+    print("Int Value : %d" %myint)
